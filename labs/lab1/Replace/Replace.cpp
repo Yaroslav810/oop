cf6e973c79bccb5903d80d52c6bc4dd5851ffa50
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-02-27 23:30:14 +0300

    Коммит потерянного файла

--- labs/lab1/Replace/Replace.cpp
+++ labs/lab1/Replace/Replace.cpp
@@ -1,10 +1,59 @@
 // Replace.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
-std::string ReplaceString(const std::string& subject,
-	const std::string& searchString, const std::string& replacementString)
+struct Args
+{
+	std::string inputFile;
+	std::string outputFile;
+	std::string searchString;
+	std::string replaceString;
+};
+
+std::string buildInvalidArgcError()
+{
+	return "Invalid argument count!"
+		   "\n"
+		   "Usage: replace.exe <inputFile> <outputFile> <searchString> <replaceString>"
+		   "\n";
+}
+
+std::string buildFileReadingError()
+{
+	return "Error reading the file!"
+		   "\n"
+		   "It is possible that the file is corrupted or there are no permissions to read it"
+		   "\n";
+}
+
+std::string buildFileWritingError()
+{
+	return "Error writing the file!"
+		   "\n"
+		   "It is possible that the file is corrupted or there are no permissions to write it"
+		   "\n";
+}
+
+Args parseArgs(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		throw std::invalid_argument(buildInvalidArgcError());
+	}
+
+	return {
+		.inputFile = argv[1],
+		.outputFile = argv[2],
+		.searchString = argv[3],
+		.replaceString = argv[4]
+	};
+}
+
+std::string replaceString(const std::string& subject,
+	const std::string& searchString,
+	const std::string& replacementString)
 {
 	size_t pos = 0;
 	std::string result;
@@ -12,50 +61,71 @@ std::string ReplaceString(const std::string& subject,
 	{
 		size_t foundPos = subject.find(searchString, pos);
 		result.append(subject, pos, foundPos - pos);
-		if (foundPos != std::string::npos) 
-		{
-			result.append(replacementString);
-			pos = foundPos + searchString.length();
-		}
-		else
+		if (foundPos == std::string::npos)
 		{
 			break;
 		}
+
+		result.append(replacementString);
+		pos = foundPos + searchString.length();
 	}
+
 	return result;
 }
 
-void CopyFileWithReplace(std::istream& input, std::ostream& output,
-	const std::string& searchString, const std::string& replacementString)
+void copyFileWithReplace(std::istream& input,
+	std::ostream& output,
+	const std::string& searchString,
+	const std::string& replacementString)
 {
 	std::string line;
-
 	while (std::getline(input, line))
 	{
-		output << ReplaceString(line, searchString, replacementString) << "\n";
+		output << replaceString(line, searchString, replacementString) << std::endl;
 	}
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 5)
+	try
 	{
-		std::cout << "Invalid argument count\n"
-				  << "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		auto args = parseArgs(argc, argv);
+
+		std::ifstream inputFile;
+		inputFile.open(args.inputFile);
+		if (!inputFile.is_open()) {
+			throw std::ios_base::failure(buildFileReadingError());
+		}
+
+		std::ofstream outputFile;
+		outputFile.open(args.outputFile);
+		if (!outputFile.is_open()) {
+			throw std::ios_base::failure(buildFileWritingError());
+		}
+
+		copyFileWithReplace(inputFile, outputFile, args.searchString, args.replaceString);
+
+		if (inputFile.bad())
+		{
+			throw std::ios_base::failure(buildFileReadingError());
+		}
+
+		if (!outputFile.flush())
+		{
+			throw std::ios_base::failure(buildFileWritingError());
+		}
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::cout << e.what() << std::endl;
 		return 1;
 	}
 
-	std::ifstream inputFile;
-	inputFile.open(argv[1]);
-
-	std::ofstream outputFile;
-	outputFile.open(argv[2]);
-
-	std::string search = argv[3];
-	std::string replace = argv[4];
-
-	CopyFileWithReplace(inputFile, outputFile, search, replace);
-	outputFile.flush();
+	catch (const std::ios_base::failure& e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
