cea24f3a5b58189d5950320d064aae0699d654cd
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-05-16 09:42:53 +0300

    Пробный тест

--- labs/lab2/MiniDictionary/tests/dictionary_test.cpp
+++ labs/lab2/MiniDictionary/tests/dictionary_test.cpp
@@ -1,3 +1,16 @@
 #define CATCH_CONFIG_MAIN
 #include "../../../../catch2/catch.hpp"
 #include "../dictionary.h"
+
+TEST_CASE("dictionary")
+{
+	SECTION("Adding words")
+	{
+		Dictionary dictionary = {
+			{"hello", {"привет"}}
+		};
+
+		AddTranslation(dictionary, "buy", "пока");
+		REQUIRE(dictionary.size() == 2);
+	}
+}
