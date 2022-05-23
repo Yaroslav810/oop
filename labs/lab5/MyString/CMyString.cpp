c52e4929a133f297d309ad87888b17440c6c2cc2
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2022-05-23 09:12:47 +0300

    Реализация нескольких конструкторов программы MyString

--- labs/lab5/MyString/CMyString.cpp
+++ labs/lab5/MyString/CMyString.cpp
@@ -1,1 +1,18 @@
 #include "CMyString.h"
+
+CMyString::CMyString()
+	: m_data(new char[1])
+	, m_length(0)
+{
+	m_data[0] = '\0';
+}
+
+CMyString::CMyString(const char* pString)
+	: m_data(new char[std::strlen(pString) + 1])
+	, m_length(std::strlen(pString))
+{
+}
+
+CMyString::CMyString(const char* pString, size_t length)
+{
+}
--- labs/lab5/MyString/main.cpp
+++ labs/lab5/MyString/main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
+	char* m =  "string";
+	auto num = std::strlen(m);
+	std::cout << num << std::endl;
 	return 0;
 }
